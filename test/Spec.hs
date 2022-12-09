c80a29649c9e6c41ebbe00d1a509c5ff637c2ce8
John Maraist <from-github@maraist.org>
2022-12-09 01:00:30 -0600

    A few more warnings gone

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
-import Shiporder0
-import Shiporder1
-import Shiporder2
+import Shiporder0()
+import Shiporder1()
+import Shiporder2()
 
 main :: IO ()
 main = putStrLn "Test suite currently all compilation-based"
