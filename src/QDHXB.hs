8a051d7bf73e67d46a21a096d47944297b7ceade
John Maraist <from-github@maraist.org>
2023-01-18 11:26:26 -0600

    Omitted from last commit

--- src/QDHXB.hs
+++ src/QDHXB.hs
@@ -27,11 +27,9 @@ import Language.Haskell.TH (Q, Dec)
 import System.IO
 import Control.Monad.IO.Class
 import Text.XML.Light.Input (parseXML)
-import Text.XML.Light.Types (Content(Elem), Element(Element), QName(QName))
-import QDHXB.Internal.Generate
-import QDHXB.Internal.Input
-import QDHXB.Internal.Flatten
+import Text.XML.Light.Types (Content) -- for re-export only
 import QDHXB.Internal.XSDQ (XSDQ, runXSDQ)
+import QDHXB.Internal.Pipeline
 import QDHXB.Options
 import QDHXB.XMLLight
 
@@ -51,17 +49,3 @@ loadFile xsdFile = do
   xsd <- liftIO $ readFile' xsdFile
   let xml = parseXML xsd
   xmlToDecs $ filter isElem xml
-
--- | Convert XML `Content` into a quotation monad returning top-level
--- Haskell declarations.
-xmlToDecs :: [Content] -> XSDQ [Dec]
-xmlToDecs ((Elem (Element (QName "?xml" _ _) _ _ _)) : ds) = case ds of
-  (Elem (Element (QName "schema" _ _) _ forms _) : []) -> do
-    schemaReps <- encodeSchemaItems forms
-    -- liftIO $ putStrLn $ "====== REPS\n" ++ show schemaReps ++ "\n======\n"
-    ir <- flattenSchemaItems schemaReps
-    -- liftIO $ putStrLn $ "====== IR\n" ++ show ir ++ "\n======\n"
-    decls <- xsdDeclsToHaskell ir
-    return decls
-  _ -> error "Expected top-level <schema> element"
-xmlToDecs _ = error "Missing <?xml> element"
