151f1c605ae42acda705438121bbb03f569272ed
John Maraist <from-github@maraist.org>
2023-01-19 15:32:06 -0600

    Setting up to debug Shiporder2

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,14 @@ import Test.TLT
 -- import Control.Monad.IO.Class
 import Shiporder0 (testShiporder0)
 import Shiporder1 (testShiporder1)
-import Shiporder2 ()
+import Shiporder2 (testShiporder2)
 
 main :: IO ()
 main = do
   tlt $ do
     testShiporder0
     testShiporder1
+    testShiporder2
 {-
     inGroup "JTMS tests" $ runJTMST $ do
       testEx1
