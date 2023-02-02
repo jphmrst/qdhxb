56002d6a7612f347bed70cd459777863dda7903f
John Maraist <from-github@maraist.org>
2023-02-01 22:06:35 -0600

    Fix issue with test itself

--- test/SizesNoList.hs
+++ test/SizesNoList.hs
@@ -5,14 +5,15 @@ import Control.Monad.Trans.Class
 import Test.TLT
 import QDHXB
 
--- qdhxb useDebugging ["sizesNoList.xsd"]
-qdhxb' ["sizesNoList.xsd"]
+qdhxb useDebugging ["sizesNoList.xsd"]
+-- qdhxb' ["sizesNoList.xsd"]
 
 testSizesNoList :: TLT IO ()
 testSizesNoList = inGroup "XSD sizesNoList" $ do
   inGroup "SizesNoList 0a" $ do
     p <- lift $ loadSizes "sizesNoList0a.xml"
-    lift $ putStrLn $ show p
+    "Correctly decode <sizes>12</sizes> in sizesNoList0a.xml" ~:
+      SizeTypeInteger 12 @==- p
   {-
   inGroup "SizesNoList 1" $ do
     p <- lift $ loadSizes "sizesNoList1.xml"
