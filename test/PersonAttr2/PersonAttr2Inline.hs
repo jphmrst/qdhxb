d8f991f6dd9b9069aab96af8e94364be436c6fbf
John Maraist <from-github@maraist.org>
2023-03-08 01:27:57 -0600

    Failing test for nested attribute types

---
+++ test/PersonAttr2/PersonAttr2Inline.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module PersonAttr2.PersonAttr2Inline where
+import Control.Monad.Trans.Class
+import Test.TLT
+import QDHXB
+import qualified QDHXB.Expansions
+
+-- qdhxb useDebugging ["test/PersonAttr2/personattr2inline.xsd"]
+qdhxb' ["test/PersonAttr2/personattr2inline.xsd"]
+
+testPersonAttr2Inline :: TLT IO ()
+testPersonAttr2Inline = inGroup "XSD PersonAttr 2" $ do
+  inGroup "PersonAttr 2 - inline" $ do
+    p <- lift $ loadPerson "test/PersonAttr1/personattr1.xml"
+    -- lift $ putStrLn $ show p
+    "Correctly decode <person> in personattr1.xml"
+      ~: PersonType (PersonattrAttrType (Just "Alpha") (Just 4)) @==- p
+  inGroup "PersonAttr 1-M" $ do
+    p <- lift $ loadPerson "test/PersonAttr1/personattr1m.xml"
+    "Correctly decode <person> in personattr1m.xml"
+      ~: PersonType (PersonattrAttrType Nothing (Just 4)) @==- p
