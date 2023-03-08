34ea176f4e38331f5e385022e5a55d652c8ae788
John Maraist <from-github@maraist.org>
2023-03-08 01:00:09 -0600

    SizesNoList tests in their own directory

--- test/SizesNoList/SizesNoList.hs
+++ test/SizesNoList/SizesNoList.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module SizesNoList where
+module SizesNoList.SizesNoList where
 import Control.Monad.Trans.Class
 import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
 
--- qdhxb useDebugging ["test/sizesNoList.xsd"]
-qdhxb' ["test/sizesNoList.xsd"]
+-- qdhxb useDebugging ["test/SizesNoList/sizesNoList.xsd"]
+qdhxb' ["test/SizesNoList/sizesNoList.xsd"]
 
 testSizesNoList :: TLT IO ()
 testSizesNoList = inGroup "XSD sizesNoList" $ do
   inGroup "SizesNoList 0a" $ do
-    p <- lift $ loadSizes "test/sizesNoList0a.xml"
+    p <- lift $ loadSizes "test/SizesNoList/sizesNoList0a.xml"
     "Correctly decode <sizes>12</sizes> in sizesNoList0a.xml" ~:
       SizeTypeTop2Union1SimpleRestrinteger 12 @==- p
   inGroup "SizesNoList 0b" $ do
-    p <- lift $ loadSizes "test/sizesNoList0b.xml"
+    p <- lift $ loadSizes "test/SizesNoList/sizesNoList0b.xml"
     "Correctly decode <sizes>small</sizes> in sizesNoList0a.xml" ~:
       SizeTypeTop2Union2SimpleRestrtoken "small" @==- p
