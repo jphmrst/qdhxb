16e9ce2400cafe4cec5fbd64822c43b854b58b91
John Maraist <from-github@maraist.org>
2023-03-08 01:13:26 -0600

    Initials tests in their own directory

--- test/Initials/Initials.hs
+++ test/Initials/Initials.hs
@@ -1,22 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Initials where
+module Initials.Initials where
 import Control.Monad.Trans.Class
 import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
 
--- qdhxb useDebugging ["test/initials.xsd"]
-qdhxb' ["test/initials.xsd"]
+-- qdhxb useDebugging ["test/Initials/initials.xsd"]
+qdhxb' ["test/Initials/initials.xsd"]
 
 testInitials :: TLT IO ()
 testInitials = inGroup "XSD initials" $ do
   inGroup "Initials 1" $ do
-    p <- lift $ loadInitials "test/initials1.xml"
+    p <- lift $ loadInitials "test/Initials/initials1.xml"
     "Correctly decode <initials> in initials1.xml" ~: "JPM" @==- p
   inGroup "Initials 2" $ do
-    p <- lift $ loadInitials "test/initials2.xml"
+    p <- lift $ loadInitials "test/Initials/initials2.xml"
     "Correctly decode <initials> in initials2.xml" ~: "MJM" @==- p
   inGroup "Initials X" $ do
-    p <- lift $ loadInitials "test/initialsX.xml"
+    p <- lift $ loadInitials "test/Initials/initialsX.xml"
     "Correctly decode <initials> in initials2.xml" ~: "JM" @==- p
