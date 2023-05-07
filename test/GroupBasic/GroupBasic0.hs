5eee3ef54c1351ae2d0807dd3eccd72cacc8790d
John Maraist <from-github@maraist.org>
2023-05-06 22:02:59 -0500

    Debugging <group> --- test without attribute

--- test/GroupBasic/GroupBasic0.hs
+++ test/GroupBasic/GroupBasic0.hs
@@ -11,9 +11,13 @@ qdhxb' ["test/GroupBasic/group-basic0.xsd"]
 
 testGB0 :: TLT IO ()
 testGB0 = inGroup "Group basic 0" $ do
-  lift $ putStrLn "Group basic 0"
   inGroup "GB0 - A" $ do
     p <- lift $ loadCombo "test/GroupBasic/gb0a.xml"
-    lift $ putStrLn $ show p
-    "Correctly decode <age> in age1.xml" ~:
+    -- lift $ putStrLn $ show p
+    "Correctly decode <group> in gb0a.xml" ~:
       (ComboType (Just "status-test") (NumsGroup 44 "z44")) @==- p
+  inGroup "GB0 - B" $ do
+    p <- lift $ loadCombo "test/GroupBasic/gb0b.xml"
+    -- lift $ putStrLn $ show p
+    "Correctly decode <group> in gb0b.xml" ~:
+      (ComboType Nothing (NumsGroup 44 "z44")) @==- p
