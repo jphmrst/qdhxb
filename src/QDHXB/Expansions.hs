d641302c6a372d77529198c6670ca3c0f6a71db0
John Maraist <from-github@maraist.org>
2023-05-09 17:53:51 -0500

    Additional names in expansion export

--- src/QDHXB/Expansions.hs
+++ src/QDHXB/Expansions.hs
@@ -6,18 +6,23 @@ module QDHXB.Expansions (
   Content,
   ZeroOneMany(Zero, One, Many), zomToList, __loadElement, pullContentFrom,
   Except, throwError, catchError, runExcept,
-  spaceSep,
+  spaceSep, pullAttrFrom,
+  Day, TimeOfDay, DiffTime, ZonedTime,
   HXBErr(..), HXBExcept,
-  bpp
+  bpp, QName
   )
 where
 
 import Control.Monad.Except (Except, throwError, runExcept, catchError)
 import Text.Read (readMaybe)
-import Text.XML.Light.Types (Content)
+import Text.XML.Light.Types (Content, QName)
+import Data.Time.Calendar.OrdinalDate (Day)
+import Data.Time.LocalTime (TimeOfDay, ZonedTime)
+import Data.Time.Clock (DiffTime)
 import QDHXB.Errs
 import QDHXB.Internal.Utils.ZeroOneMany
-import QDHXB.Internal.Utils.XMLLight (__loadElement, pullContentFrom)
+import QDHXB.Internal.Utils.XMLLight (
+  __loadElement, pullContentFrom, pullAttrFrom)
 import QDHXB.Internal.Utils.Misc (spaceSep)
 import QDHXB.Internal.Utils.BPP (bpp)
 import QDHXB.Internal.Generate (simpleTypeDecoder)
