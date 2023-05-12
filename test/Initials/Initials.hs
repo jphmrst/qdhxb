9aa8cb4b67a81dd6859862fccc0f5556f8a37867
John Maraist <from-github@maraist.org>
2023-05-12 00:00:39 -0500

    This test works as-is with current rebuild.

--- test/Initials/Initials.hs
+++ test/Initials/Initials.hs
@@ -7,11 +7,10 @@ import QDHXB
 import qualified QDHXB.Expansions
 
 -- qdhxb useDebugging ["test/Initials/initials.xsd"]
--- qdhxb' ["test/Initials/initials.xsd"]
+qdhxb' ["test/Initials/initials.xsd"]
 
 testInitials :: TLT IO ()
 testInitials = inGroup "XSD initials" $ do
-  {-
   inGroup "Initials 1" $ do
     p <- lift $ loadInitials "test/Initials/initials1.xml"
     "Correctly decode <initials> in initials1.xml" ~: "JPM" @==- p
@@ -21,6 +20,5 @@ testInitials = inGroup "XSD initials" $ do
   inGroup "Initials X" $ do
     p <- lift $ loadInitials "test/Initials/initialsX.xml"
     "Correctly decode <initials> in initials2.xml" ~: "JM" @==- p
-  -}
   return ()
 
