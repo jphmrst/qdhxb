86a54be915c9f7e9eec42e601179c7056b49b1ec
John Maraist <from-github@maraist.org>
2023-05-12 00:04:32 -0500

    Script output tweaks

--- show-statuses.sh
+++ show-statuses.sh
@@ -5,7 +5,11 @@ grep ^qdhxb\'\  test/*/*.hs
 echo --
 grep ^qdhxb\  test/*/*.hs 
 echo --
-ls test/*/*.hs 
+grep -l ^qdhxb\'\  test/*/*.hs  > .tmp-files
+grep -l ^qdhxb\  test/*/*.hs   >> .tmp-files
+ls test/*/*.hs > .tmp-all
+grep -v -f .tmp-files .tmp-all
+rm -f .tmp-files .tmp-all
 echo --
 wc -l test/*/*.xsd | sort -n
 echo --
