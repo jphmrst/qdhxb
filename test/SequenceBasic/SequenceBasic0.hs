1bde4b0ce6f3c8872408c6df5df7752948f90b40
John Maraist <from-github@maraist.org>
2023-05-17 11:48:48 -0500

    Adding clause to SequenceBasic0 --- passing

--- test/SequenceBasic/SequenceBasic0.hs
+++ test/SequenceBasic/SequenceBasic0.hs
@@ -11,9 +11,7 @@ qdhxb' ["test/SequenceBasic/seqbasic0.xsd"]
 
 testSequencebasic0 :: TLT IO ()
 testSequencebasic0 = inGroup "XSD seqbasic0" $ do
-  {-
   "Correctly decode <orderperson> text in seqbasic0a.xml" ~:
     (Seqbasic "xyz789" 59) @==
       (lift $ loadSeqbasic "test/SequenceBasic/seqbasic0a.xml")
-  -}
   return ()
