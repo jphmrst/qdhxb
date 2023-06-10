b7e87bd33a4a2371d6a37097c131756c204ed191
John Maraist <from-github@maraist.org>
2023-06-10 18:33:30 -0500

    PersonAttr2Inline passing

--- test/PersonAttr2/PersonAttr2Inline.hs
+++ test/PersonAttr2/PersonAttr2Inline.hs
@@ -6,20 +6,18 @@ import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
 
-qdhxb (useDebugging . logByFile True) ["test/PersonAttr2/personattr2inline.xsd"]
--- qdhxb' ["test/PersonAttr2/personattr2inline.xsd"]
+-- qdhxb (useDebugging . logByFile True) ["test/PersonAttr2/personattr2inline.xsd"]
+qdhxb' ["test/PersonAttr2/personattr2inline.xsd"]
 
 testPersonAttr2Inline :: TLT IO ()
 testPersonAttr2Inline = inGroup "XSD PersonAttr 2" $ do
-  {-
   inGroup "PersonAttr 2 - inline" $ do
     p <- lift $ loadPerson "test/PersonAttr1/personattr1.xml"
     -- lift $ putStrLn $ show p
     "Correctly decode <person> in personattr1.xml"
-      ~: PersonType (PersonattrAttrType (Just "Alpha") (Just 4)) @==- p
+      ~: PersonType (Just (Personattr (Just "Alpha") (Just 4))) @==- p
   inGroup "PersonAttr 1-M" $ do
     p <- lift $ loadPerson "test/PersonAttr1/personattr1m.xml"
     "Correctly decode <person> in personattr1m.xml"
-      ~: PersonType (PersonattrAttrType Nothing (Just 4)) @==- p
-  -}
+      ~: PersonType (Just $ Personattr Nothing $ Just 4) @==- p
   return ()
