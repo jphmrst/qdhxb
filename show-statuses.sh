3b5f54b0f2f679814462425bcaa839d87d95ef43
John Maraist <from-github@maraist.org>
2023-06-10 22:34:04 -0500

    Dig for whether XSDs are used

--- show-statuses.sh
+++ show-statuses.sh
@@ -2,17 +2,26 @@
 
 echo --
 grep ^qdhxb\'\  test/*/*.hs 
+
 echo --
 grep ^qdhxb\  test/*/*.hs 
+
 echo --
 grep -l ^qdhxb\'\  test/*/*.hs  > .tmp-files
 grep -l ^qdhxb\  test/*/*.hs   >> .tmp-files
 ls test/*/*.hs > .tmp-all
 grep -v -f .tmp-files .tmp-all > .tmp-nonfinished
-grep TODO `cat .tmp-nonfinished`
+grep TODO `cat .tmp-nonfinished` | sort -k 3
 grep -l TODO `cat .tmp-nonfinished` > .tmp-nonfinished-withTODO
 grep -v -f .tmp-nonfinished-withTODO .tmp-nonfinished
 rm -f .tmp-files .tmp-all .tmp-nonfinished .tmp-nonfinished-withTODO
+
 echo --
-wc -l test/*/*.xsd | sort -n
+grep qdhxb test/*/*.hs | sed -e 's/^.*\["//' | sed -e 's/"\]$//' | sort -u > .tmp-qdhxb-calls
+ls -1 test/*/*.xsd > .tmp-all-xsd
+grep -v -f .tmp-qdhxb-calls .tmp-all-xsd > .tmp-nonfinished
+# cat .tmp-qdhxb-calls
+(for m in `cat .tmp-nonfinished`; do wc -l $m; done) | sort -n
+rm .tmp-all-xsd .tmp-nonfinished .tmp-qdhxb-calls
+
 echo --
