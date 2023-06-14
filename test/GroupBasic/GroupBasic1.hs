0c30019d882571586f29ccc792e58125e1869979
John Maraist <from-github@maraist.org>
2023-06-14 16:11:15 -0500

    Group-basic-1 test passing, although it is just a <group> so no actual tests, just the compile

--- test/GroupBasic/GroupBasic1.hs
+++ test/GroupBasic/GroupBasic1.hs
@@ -6,18 +6,8 @@ import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
 
--- TODO Type error in generated code
 -- qdhxb (useDebugging . logByFile True) ["test/GroupBasic/group-basic1.xsd"]
--- qdhxb' ["test/GroupBasic/group-basic1.xsd"]
+qdhxb' ["test/GroupBasic/group-basic1.xsd"]
 
 testGB1 :: TLT IO ()
 testGB1 = return ()
-{-
-inGroup "XSD age" $ do
-  inGroup "Age 1" $ do
-    p <- lift $ loadAge "test/GroupBasic/group-basic01.xml"
-    "Correctly decode <age> in age1.xml" ~: 10 @==- p
-  inGroup "Age 2" $ do
-    p <- lift $ loadAge "test/GroupBasic/group-basic02.xml"
-    "Correctly decode <age> in age2.xml" ~: 55 @==- p
--}
