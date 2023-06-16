d76f58d4856c42498a56f52b3f97f59b907fc3ec
John Maraist <from-github@maraist.org>
2023-06-16 15:13:14 -0500

    Books1 now working

--- test/Books/Books1.hs
+++ test/Books/Books1.hs
@@ -5,11 +5,16 @@ import Control.Monad.Trans.Class
 import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
+import Data.Time.Calendar (fromGregorian)
 
--- TODO Reaches REDO in Generate
 -- qdhxb (useDebugging . logByFile True) ["test/Books/books.xsd"]
--- qdhxb' ["test/Books/books.xsd"]
+qdhxb' ["test/Books/books.xsd"]
 
 testBooks1 :: TLT IO ()
 testBooks1 = inGroup "XSD books 1" $ do
+  "Correctly decode single <book> in book1.xml" ~:
+    (BookForm (Just "bk001") "Writer" "The First Book" "Fiction"
+              44.95 (fromGregorian 2000 10 1)
+              "An amazing story of nothing.")
+      @== (lift $ loadBook "test/Books/book1.xml")
   return ()
