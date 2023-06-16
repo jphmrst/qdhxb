7b351edd1549d45f4d0b4e83da9822ab233698ed
John Maraist <from-github@maraist.org>
2023-06-16 01:09:13 -0500

    Remove some of the "test/" from summary output

--- show-statuses.sh
+++ show-statuses.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 echo --
-grep ^qdhxb\'\  test/*/*.hs 
+grep ^qdhxb\'\  test/*/*.hs | sed -e 's|test/||g'
 
 echo --
-grep ^qdhxb\  test/*/*.hs 
+grep ^qdhxb\  test/*/*.hs | sed -e 's|test/||g'
 
 echo --
 grep -l ^qdhxb\'\  test/*/*.hs  > .tmp-files
 grep -l ^qdhxb\  test/*/*.hs   >> .tmp-files
 ls test/*/*.hs > .tmp-all
 grep -v -f .tmp-files .tmp-all > .tmp-nonfinished
-grep TODO `cat .tmp-nonfinished` | sort -k 3
+grep TODO `cat .tmp-nonfinished` | sort -k 3 | sed -e 's|test/||g'
 grep -l TODO `cat .tmp-nonfinished` > .tmp-nonfinished-withTODO
 grep -v -f .tmp-nonfinished-withTODO .tmp-nonfinished
 rm -f .tmp-files .tmp-all .tmp-nonfinished .tmp-nonfinished-withTODO
