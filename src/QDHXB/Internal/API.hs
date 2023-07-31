dc3851f199bd9a2d7834b606968f9bfa7c1ed798
John Maraist <from-github@maraist.org>
2023-07-31 13:58:09 -0500

    More boilerplate erasing

---
+++ src/QDHXB/Internal/API.hs
@@ -0,0 +1,56 @@
+
+-- | Top-level calls
+module QDHXB.Internal.API (apiFunctions, API) where
+
+import Language.Haskell.TH (Q, Dec)
+import System.IO
+import Control.Monad.IO.Class
+import Data.List (intercalate)
+import Text.XML.Light.Types (Content(Elem))
+import Text.XML.Light.Input (parseXML)
+import QDHXB.Utils.XMLLight (isElem)
+import QDHXB.Internal.XSDQ (
+  XSDQ, runXSDQ, whenResetLog,
+  localLoggingStart, localLoggingEnd, putLog, resetLog, whenLogging)
+import QDHXB.Internal.AST
+import QDHXB.Internal.Pipeline
+import QDHXB.Internal.Types
+import QDHXB.Options
+
+-- | Shorthand for the two top-level functions based on a given XSD
+-- `AST` implementation.
+type API = (QDHXBOption -> [String] -> Q [Dec], [String] -> Q [Dec])
+
+-- | Define the two API functions (with and without options) for a
+-- given XSD `AST` implementation.
+apiFunctions :: AST ast =>
+  (String -> [Content] -> XSDQ [ast]) -> ([ast] -> XSDQ [Definition]) -> API
+apiFunctions inputSchemaItems flattenSchemaItems = (qdhxbFn, qdhxbFn')
+  where -- | Load the given XSD files, translating each into Haskell
+        -- declarations.
+        qdhxbFn :: QDHXBOption -> [String] -> Q [Dec]
+        qdhxbFn opts xsds = do
+          -- liftIO (getCurrentDirectory >>= putStrLn . show)
+          runXSDQ opts $ do
+            whenLogging $ \file -> do
+              whenResetLog $ resetLog file
+              liftIO $ appendFile file $
+                "Files: " ++ intercalate ", " xsds ++ "\n"
+            xsdContents <- mapM load_content xsds
+            translateParsedXSD inputSchemaItems flattenSchemaItems xsdContents
+
+        -- | Load and translate the given XSD files with the default
+        -- options.
+        qdhxbFn' :: [String] -> Q [Dec]
+        qdhxbFn' = qdhxbFn id
+
+load_content :: String -> XSDQ [Content]
+load_content xsdFile = do
+  localLoggingStart
+  putLog $ "============================== " ++ xsdFile ++ "\n"
+  xsd <- liftIO $ readFile' xsdFile
+  let xml :: [Content]
+      xml = parseXML xsd
+  let res = filter isElem xml
+  localLoggingEnd
+  return res
--- src/QDHXB/Internal/L0/API.hs
+++ src/QDHXB/Internal/L0/API.hs
@@ -3,43 +3,14 @@
 module QDHXB.Internal.L0.API (qdhxb, qdhxb') where
 
 import Language.Haskell.TH (Q, Dec)
-import System.IO
-import Control.Monad.IO.Class
-import Data.List (intercalate)
-import Text.XML.Light.Types (Content(Elem))
-import Text.XML.Light.Input (parseXML)
-import QDHXB.Utils.XMLLight (isElem)
-import QDHXB.Internal.XSDQ (
-  XSDQ, runXSDQ, whenDebugging, whenResetLog,
-  localLoggingStart, localLoggingEnd, putLog, resetLog, whenLogging)
+import QDHXB.Options
+import QDHXB.Internal.API
 import QDHXB.Internal.L0.Input
 import QDHXB.Internal.L0.Flatten
-import QDHXB.Internal.Pipeline
-import QDHXB.Options
 
 -- | Load the given XSD files, translating each into Haskell
 -- declarations.
 qdhxb :: QDHXBOption -> [String] -> Q [Dec]
-qdhxb opts xsds = do
-  -- liftIO (getCurrentDirectory >>= putStrLn . show)
-  runXSDQ opts $ do
-    whenLogging $ \file -> do
-      whenResetLog $ resetLog file
-      liftIO $ appendFile file $ "Files: " ++ intercalate ", " xsds ++ "\n"
-    xsdContents <- mapM loadContent xsds
-    translateParsedXSD inputSchemaItems flattenSchemaItems xsdContents
-
 -- | Load and translate the given XSD files with the default options.
 qdhxb' :: [String] -> Q [Dec]
-qdhxb' = qdhxb id
-
-loadContent :: String -> XSDQ [Content]
-loadContent xsdFile = do
-  localLoggingStart
-  putLog $ "============================== " ++ xsdFile ++ "\n"
-  xsd <- liftIO $ readFile' xsdFile
-  let xml :: [Content]
-      xml = parseXML xsd
-  let res = filter isElem xml
-  localLoggingEnd
-  return res
+(qdhxb, qdhxb') = apiFunctions inputSchemaItems flattenSchemaItems
