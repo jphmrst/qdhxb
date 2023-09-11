037838cca2533d729ce404bbb5da7f796aabbb7e
John Maraist <from-github@maraist.org>
2023-09-11 15:01:19 -0500

    Test passing, but with note for more test cases

--- test/CustomersOrders/CustomersOrders1.hs
+++ test/CustomersOrders/CustomersOrders1.hs
@@ -5,32 +5,21 @@ import Control.Monad.Trans.Class
 import Test.TLT
 import QDHXB
 import qualified QDHXB.Expansions
-import Data.Symbol
 
--- TODO ZonedTime not declared Eq? (previously: same name in different XML scopes)
--- qdhxb (setDebugging (intern "unique") 3) ["test/CustomersOrders/customersorders.xsd"]
--- qdhxb' ["test/CustomersOrders/customersorders.xsd"]
+-- TODO Write test cases --- might be a problem, but might be with the big MS XML file.
+-- import QDHXB.Internal.Debugln
+-- qdhxb (setDebugging generate 3) ["test/CustomersOrders/customersorders.xsd"]
+qdhxb' ["test/CustomersOrders/customersorders.xsd"]
 
 testCustomersOrders1 :: TLT IO ()
 testCustomersOrders1 = inGroup "XSD customers/orders 1" $ do
-  return ()
   {-
-  inGroup "Scalars" $ do
-    "Correctly decode <orderperson> text in shiporder0a.xml" ~:
-      "John Smith" @== (lift $ loadOrderperson "test/shiporder0a.xml")
-    "Correctly decode <price> text in shiporder0b.xml" ~:
-      10.9 @== (lift $ loadPrice "test/shiporder0b.xml")
-  inGroup "Structures" $ do
-    do p <- lift $ loadShipto "test/shiporder0c.xml"
-       "Correctly decode <shipto> in shiporder0c.xml" ~:
-         Shipto "Ola Nordmann" "Langgt 23" "4000 Stavanger" "Norway"
-           @==- p
-    do p <- lift $ loadShiporder "test/shiporder1a.xml"
-       "Correctly decode <shiporder> in shiporder1a.xml" ~:
-         (Shiporder "889923" "John Smith"
-           (Shipto "Ola Nordmann" "Langgt 23" "4000 Stavanger" "Norway")
-           [ Item "Empire Burlesque" (Just "Special Edition") 1 10.9,
-             Item "Hide your heart" Nothing 1 9.9
-           ])
-         @==- p
--}
+  inGroup "Load big file" $ do
+    root <- lift $ loadRoot "test/CustomersOrders/customersorders.xml"
+    case root of
+      Root (Customers cs) (Orders os) -> do
+        lift $ putStrLn $
+          (show $ length cs) ++ " customers, "
+          ++ (show $ length os) ++ " orders"
+  -}
+  return ()
