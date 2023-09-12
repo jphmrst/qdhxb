9b979f8cd9dcffcc9918f6b362cfaf1d77301e92
John Maraist <from-github@maraist.org>
2023-09-11 22:57:21 -0500

    Setting up smaller examples of the current bug --- omitted form last commit

---
+++ test/GroupBasic/GroupRefs3.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module GroupBasic.GroupRefs3 where
+import Control.Monad.Trans.Class
+import Test.TLT
+import QDHXB
+import qualified QDHXB.Expansions
+
+{-
+import QDHXB.Internal.Debugln
+qdhxb (setDebugging generate 3) ["test/GroupBasic/group-refs.xsd"]
+-}
+qdhxb' ["test/GroupBasic/group-refs.xsd"]
+
+testGB3 :: TLT IO ()
+testGB3 = do
+  return ()
---
+++ test/GroupBasic/GroupRefs4.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module GroupBasic.GroupRefs4 where
+import Control.Monad.Trans.Class
+import Test.TLT
+import QDHXB
+import qualified QDHXB.Expansions
+
+{-
+import QDHXB.Internal.Debugln
+qdhxb (setDebugging generate 3) ["test/GroupBasic/group-ref2.xsd"]
+-}
+qdhxb' ["test/GroupBasic/group-refs2.xsd"]
+
+testGB4 :: TLT IO ()
+testGB4 = do
+  return ()
