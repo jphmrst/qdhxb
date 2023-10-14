66295f304459660788e041b6017dab3ee0a36d8c
John Maraist <from-github@maraist.org>
2023-10-14 01:02:53 -0500

    Some test calls

--- src/QDHXB/Internal/L1/NestedTypes.hs
+++ src/QDHXB/Internal/L1/NestedTypes.hs
@@ -6,15 +6,24 @@ module QDHXB.Internal.L1.NestedTypes(victory) where
 import QDHXB.Internal.L0
 import QDHXB.Options
 import qualified QDHXB.Expansions
--- import qualified QDHXB.Internal.L1.XML
+import qualified QDHXB.Internal.L1.XML
 
--- qdhxb (useXmlBindings . withXmlNamespacePrefix "xml"
---         . useDebugging
---         . logToFile "nestedTypes.log"
---       ) [
---   "src/QDHXB/datatypes.xsd",
---   "src/QDHXB/xsd.xsd"
---   ]
+{-
+qdhxb (withXmlNamespacePrefix "xml"
+      ) [
+  "src/QDHXB/datatypes.xsd",
+  "src/QDHXB/xsd.xsd"
+  ]
+-}
+
+{-
+import QDHXB.Internal.Debugln
+qdhxb (setDebugging input 1
+       . setDebugging unique 1
+       . setDebugging flattening 1
+       . setDebugging generate 1)
+  ["src/QDHXB/datatypes.xsd"]
+-}
 
 -- |Placeholder, until the full XSD specification can actually be
 -- generated.
