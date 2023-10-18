058c6d5c79dc813b4136d9e1ddde43d3f8c87c60
John Maraist <from-github@maraist.org>
2023-10-18 12:50:41 -0500

    Loading notes

--- src/QDHXB/Internal/L1/NestedTypes.hs
+++ src/QDHXB/Internal/L1/NestedTypes.hs
@@ -8,22 +8,26 @@ import QDHXB.Options
 import qualified QDHXB.Expansions
 import qualified QDHXB.Internal.L1.XML
 
+
+-- Note that datatypes.xsd and xsd.xsd are mutually dependent, so both
+-- must be loaded at the same call to `qdhxb`.  Recall (see ) that all
+-- files are first parsed and flattened, and then all generated
+-- together, so that all types etc. in the loaded files are known at
+-- the final generation phase.
+
 {-
 qdhxb (withXmlNamespacePrefix "xml"
       ) [
-  "src/QDHXB/datatypes.xsd",
-  "src/QDHXB/xsd.xsd"
+  "src/QDHXB/datatypes.xsd", "src/QDHXB/xsd.xsd"
   ]
 -}
 
-{-
 import QDHXB.Internal.Debugln
-qdhxb (setDebugging input 1
-       . setDebugging unique 1
-       . setDebugging flattening 1
-       . setDebugging generate 1)
-  ["src/QDHXB/datatypes.xsd"]
--}
+qdhxb (setDebugging input 1 .
+        --setDebugging unique 1 .
+        --setDebugging flattening 1 .
+        setDebugging generate 1)
+  ["src/QDHXB/xsd.xsd", "src/QDHXB/datatypes.xsd"]
 
 -- |Placeholder, until the full XSD specification can actually be
 -- generated.
